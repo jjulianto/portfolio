8641e3375ff795d02ad4236652e3ad83321b3f99
Julianto08 <jjulianto08@gmail.com>
2021-07-14 02:24:55 +0700

    feat: add portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,3 +82,17 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+let swiper = new Swiper('.portfolio-container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
