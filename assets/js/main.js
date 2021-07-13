9d565ae7ab5c7ce17fe79cf597d63036a2bdbb7b
Julianto08 <jjulianto08@gmail.com>
2021-07-13 13:27:27 +0700

    feat: add skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,21 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+const skillsContent = document.getElementsByClassName('skills-content');
+const skillsHeader = document.querySelectorAll('.skills-header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for(i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills-content skills-close';
+    }
+    if(itemClass === 'skills-content skills-close') {
+        this.parentNode.className = 'skills-content skills-open';
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+})
