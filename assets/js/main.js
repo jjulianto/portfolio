40ee61afc54671a705ec2c53c8e020e9d8afef65
Julianto08 <jjulianto08@gmail.com>
2021-07-13 16:01:47 +0700

    feat: add qualification section

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,23 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills);
 })
+
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification-active');
+        })
+        target.classList.add('qualification-active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        })
+        tab.classList.add('active');
+
+    })
+})
