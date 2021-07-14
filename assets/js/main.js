817609b559515a3b5013a05e7c3143920270d89b
Julianto08 <jjulianto08@gmail.com>
2021-07-15 01:58:37 +0700

    feat: add scroll up button when the scroll is higher than 560 viewport height

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,3 +122,10 @@ function scrollHeader(){
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
